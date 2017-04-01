f54ade3e4ab7b8def8a15c089b2a0f645b1cd02f
Julian Scholle <julian.scholle@googlemail.com>
2017-04-01 04:12:06 +0200

    sync

--- code/vrep/eval.py
+++ code/vrep/eval.py
@@ -12,6 +12,11 @@ import matplotlib.pyplot as plt
 measurements = pickle.load(open("measurements.p", "rb"))    #[self.globals.iteration, self.pos, self.speed, self.theta, self.type]
 simulation = pickle.load(open("simulation.p", "rb"))        #[self.iteration, enemy.getPosition(), enemy.speed, enemy.getOriantation(), enemy.type]
 
+simulation["Car0"] = simulation.pop("enemy_car1")
+simulation["Car1"] = simulation.pop("enemy_car2")
+simulation["Car2"] = simulation.pop("enemy_car3")
+simulation["Bike2"] = simulation.pop("bike")
+simulation["Pedestrian"] = simulation.pop("Bill_base")
 
 print measurements.keys()
 print simulation.keys()
@@ -33,43 +38,90 @@ for key in measurements.keys():
             break
 
 
-print mapping
 
+
+
+if True:
+
+
+    pylab.figure(1)
+    for sim_key in simulation.keys():
+        if sim_key != "mycar":
+            print sim_key
+
+
+
+            for mess in mapping[sim_key]:
+                times= list()
+                distances = list()
+
+
+                pylab.subplot(211)
+
+                for data in measurements[mess]:
+                    dist = np.linalg.norm(np.array(data[1][:2]) - np.array(simulation[sim_key][data[0]][1][:2]))
+                    #print np.array(simulation[sim_key][data[0]][1])
+                    if data[0] < 150:
+                        times.append(data[0]/10.0)
+                        distances.append(dist*10)
+                if measurements[mess][0][0] < 150 and len(measurements[mess])>2:
+                    pylab.plot(times,distances,label=str(mess))
+
+                speed = list()
+                pylab.figure(1)
+                pylab.subplot(212)
+                for data in measurements[mess]:
+                    dist = np.array(data[2])  # - np.array(simulation[sim_key][data[0]][2])
+                    if data[0] < 150:
+                        #times.append(data[0] / 10.0)
+                        speed.append(dist * 10)
+                if measurements[mess][0][0] < 150 and len(measurements[mess]) > 2:
+                    pylab.plot(times, speed, label=str(mess))
+
+
+
+
+            pylab.subplot(211)
+            pylab.ylabel("error [m]")
+            ##pylab.xlabel("time [s]")
+            pylab.title(str(sim_key))
+            pylab.legend(loc='upper left')
+
+            pylab.subplot(212)
+
+            pylab.ylabel("speed [m/s]")
+            pylab.xlabel("time [s]")
+            pylab.legend(loc='upper left')
+            pylab.show()
+
+pylab.figure(2)
 for sim_key in simulation.keys():
-    print sim_key
+    if sim_key != "mycar":
+        print sim_key
 
-    for mess in mapping[sim_key]:
-        times= list()
-        distances = list()
+        xval = list()
+        yval = list()
+        for dat in simulation[sim_key][1:150]:
+            xval.append(dat[1][0])
+            yval.append(dat[1][1])
+        pylab.plot(xval, yval, label=str(sim_key))
 
-        for data in measurements[mess]:
-            dist = np.linalg.norm(np.array(data[1]) - np.array(simulation[sim_key][data[0]][1]))
-            if data[0] < 150:
-                times.append(data[0]/10.0)
-                distances.append(dist*10)
-        if measurements[mess][0][0] < 150 and len(measurements[mess])>2:
-            pylab.plot(times,distances,label=str(mess))
-    pylab.ylabel("error [m]")
-    pylab.xlabel("time [s]")
-    pylab.title(str(sim_key))
-    pylab.legend(loc='upper left')
-    pylab.show()
+        for mess in mapping[sim_key]:
+            xval = list()
+            yval = list()
 
-for sim_key in simulation.keys():
-    for mess in mapping[sim_key]:
-        times= list()
-        speed = list()
+            pylab.axis('equal')
+            for data in measurements[mess]:
+                dist = np.array(data[2])  # - np.array(simulation[sim_key][data[0]][2])
+                if data[0] < 100:
+                    xval.append(data[1][0])
+                    yval.append(data[1][1])
+            if measurements[mess][0][0] < 150 and len(measurements[mess]) > 2:
+                pylab.plot(xval, yval, label=str(mess))
 
-        for data in measurements[mess]:
-            dist = np.array(data[2]) #- np.array(simulation[sim_key][data[0]][2])
-            if data[0] < 150:
-                times.append(data[0]/10.0)
-                speed.append(dist*10)
-        if measurements[mess][0][0]<150 and len(measurements[mess])>2:
-            pylab.plot(times,speed,label=str(mess))
 
-    pylab.ylabel("speed [m/s]")
-    pylab.xlabel("time [s]")
-    pylab.title(str(sim_key))
-    pylab.legend(loc='upper left')
-    pylab.show()
+        pylab.title(str(sim_key))
+        pylab.ylabel("y-pos [m]")#
+        pylab.xlabel("x-pos [m]")
+        pylab.legend(loc='upper left')
+        pylab.show()
